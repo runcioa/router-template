2eaf7ae874f72bbb1fd78f640c585c615a291ff9
Antonio Runcio <antonio.runcio@ecoforservice.it>
2022-10-28 12:57:09 +0200

    Outlet sostituisce Children

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import {Link, Route, Routes} from 'react-router-dom'
+import { Link, Route, Routes, Outlet } from 'react-router-dom'
 
 
 function App() {
   return (
     <>
-    <h1>React Router</h1>
+      <h1>React Router</h1>
 
-    <Navigation />
-    <Routes>
-      {/* Route per il Layout applicato a tutti e due gli elementi  */}
-      <Route element={<Layout />}>
-      <Route path='/home' element={<Home />} />
-      <Route path='/users' element={<Users />} />
-      </Route>
-    </Routes>
+      <Navigation />
+      <Routes>
+        {/* Route per il Layout applicato a tutti e due gli elementi  */}
+        <Route element={<Layout />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/users' element={<Users />} />
+        </Route>
+      </Routes>
     </>
   );
 };
 
 const Navigation = () => {
-  
+
   return (
-    <nav style={{borderBottom: 'solid 1px', paddingBottom: '1rem'}}>
+    <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
       <Link to="/home">Home</Link>
       <Link to="/users">Users</Link>
     </nav>
@@ -30,25 +30,29 @@ const Navigation = () => {
   )
 };
 
-const Home = () =>{
+const Home = () => {
   return (
-      <h2>Home</h2>
-    
+    <h2>Home</h2>
+
   )
 }
 
-const Users = () =>{
+const Users = () => {
   return (
-   
-      <h2>Users</h2>
-    
+
+    <h2>Users</h2>
+
 
   )
 }
 
 // Utilizzo il Layout per gli stili di entrambe le pagine
-const Layout = ({children}) =>{
-  return ( <main style={{padding: '1rem 0', color: 'red'}}>{children}</main>
+// Con Outlet tolgo i childre e li prende da soli
+const Layout = () => {
+  return (
+    <main style={{ padding: '1rem 0', color: 'red' }}>
+      <Outlet />
+    </main>
 
   )
 }
